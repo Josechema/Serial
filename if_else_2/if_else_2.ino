0e2e35d8fff271a376e0b70c32ba53149f691b4c
Josechema <jgilcfgs@gmail.com>
2017-02-22 00:39:47 +0100

    if 2

---
+++ if_else_2/if_else_2.ino
@@ -0,0 +1,41 @@
+/*****************************************************************
+**                                                              **
+**                       TÍTOL: if / else                       **
+**                Titol: Control amb if / else                  **
+**                                                              **
+**  NOM: JOSE MARIA GIL FLORES              DATA:22/02/2017     **
+*****************************************************************/
+
+//********************* INCLUDE  *********************************
+
+
+//********************* VARIABLES ********************************
+
+int tempAigua = 90;        // temperatura aigua 
+
+//*********************** SETUP **********************************
+
+void setup() {             // configura el final de la salida
+  Serial.begin(9600); // activa el monitor serie a 9600 baudis
+
+  if ( tempAigua < 100)   // cuan la tempAigua inferior a 100
+  {
+    Serial.print("Aigua encara no bull"); // en monitor
+  }
+ else                               // sino, 
+  {
+    Serial.print("Aigua bullint");  // en monitor aigua bull
+  }
+}
+
+
+//*********************** LOOP ***********************************
+
+
+void loop () {             // inicia el bucle del programa
+
+}
+
+
+
+//*********************** FUNCIONS *******************************
